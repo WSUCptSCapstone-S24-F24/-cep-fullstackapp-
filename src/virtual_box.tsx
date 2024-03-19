387758c11d4b3df78fdcd6bb7eb89e46ca801eb5
aidangaetano11 <aidan.gaetano@wsu.edu>
2024-03-19 12:43:34 -0700

    Removed middle dot in virtual box, as well as finding distance to those center dots

--- src/virtual_box.tsx
+++ src/virtual_box.tsx
@@ -14,17 +14,14 @@ interface VirtualBoxInfo {
 
 const VirtualBox: React.FC<VirtualBoxInfo> = ({ crosshairPosition, name, height, width, top = '0', left = '0', right='0'}) => {
     const boxRef = useRef<HTMLDivElement>(null);
-    const centerDotRef = useRef<HTMLDivElement>(null);
 
     const [isInside, setIsInside] = useState(false);
-    const [distance, updateDistance] = useState<number>(0);
 
     useEffect(() =>{
-        if (!boxRef.current || !centerDotRef.current) return;
+        if (!boxRef.current) return;
 
         // Will check if the crosshair is within the bounds of the virtual box
         const box = boxRef.current.getBoundingClientRect();
-        const dot = centerDotRef.current.getBoundingClientRect();
         const isInside = 
         crosshairPosition.x >= box.left &&
         crosshairPosition.x <= box.right &&
@@ -32,21 +29,6 @@ const VirtualBox: React.FC<VirtualBoxInfo> = ({ crosshairPosition, name, height,
         crosshairPosition.y <= box.bottom;
 
         setIsInside(isInside);
-
-        if (!centerDotRef.current) return;
-        
-
-        // Will calculate the center position of the dot
-        const dotCenterX = dot.left + (dot.width) / 2;
-        const dotCenterY = dot.top + (dot.height) / 2;
-
-        // Calculate the distance between center dot and crosshair position
-        const distance = Math.sqrt(Math.pow(crosshairPosition.x - dotCenterX, 2) + Math.pow(crosshairPosition.y - dotCenterY, 2));
-
-        // Update Variables
-        updateDistance(distance);
-        
-
     }, [crosshairPosition])
 
     return (
@@ -63,23 +45,6 @@ const VirtualBox: React.FC<VirtualBoxInfo> = ({ crosshairPosition, name, height,
             zIndex: 4
           }}
         >
-            <div
-                ref={centerDotRef}
-                style={{
-                height: '10px',
-                width: '10px',
-                backgroundColor: 'blue',
-                borderRadius: '50%',
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'scale(1.3) translate(-50%, -50%)', // Centers the dot
-                }}
-            />
-            {/*Dispay distance crosshair is from center point*/}
-            <span style={{ position: 'absolute', bottom: '10px', color: 'black', fontWeight: 'bold', visibility: `${isInside ? 'visible' : 'hidden'}`}}>
-                {name + " accuracy: " + distance.toFixed(2) + " px"}
-            </span>
         </div>
     );
 };
