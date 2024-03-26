596248e07ffbc9ce9328b5a8b7411b4f12ba509c
Yuuki Matsunari <yuuki.matsunari@wsu.edu>
2024-03-26 11:48:04 -0700

    Implemented vector field by random vectors (just for testing)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
-function Home()
-{
+function Home() {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        // set SVG
+        const width = 500;
+        const height = 500;
+        const svg = d3.select(ref.current)
+                      .attr("width", width)
+                      .attr("height", height);
+
+        // Generate random vectors
+        const data = Array.from({ length: 30 }, (_, i) => ({
+            x: Math.random() * width,
+            y: Math.random() * height,
+            dx: Math.random() * 20 - 10, // X vector component
+            dy: Math.random() * 20 - 10, // Y vector component
+        }));
+
+        // Draw vector field
+        svg.selectAll(".vector")
+           .data(data)
+           .enter()
+           .append("line") // Draw line
+           .attr("x1", d => d.x)
+           .attr("y1", d => d.y)
+           .attr("x2", d => d.x + d.dx)
+           .attr("y2", d => d.y + d.dy)
+           .attr("stroke", "black")
+           .attr("marker-end", "url(#arrow)"); // Add arrow
+        // Def of arrow
+        svg.append("defs")
+           .append("marker")
+           .attr("id", "arrow")
+           .attr("viewBox", "0 -5 10 10")
+           .attr("refX", 5)
+           .attr("refY", 0)
+           .attr("markerWidth", 6)
+           .attr("markerHeight", 6)
+           .attr("orient", "auto")
+           .append("path")
+           .attr("d", "M0,-5L10,0L0,5")
+           .attr("fill", "black");
+    }, []);
+
     return (
         <div>
             <h1>Hello World!</h1>
+            <svg ref={ref}></svg>
         </div>
-        
     );
 }
 
