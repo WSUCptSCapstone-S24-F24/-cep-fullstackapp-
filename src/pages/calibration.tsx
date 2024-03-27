c3b2df2f3b0e0bbe99a66d72a2a51dd13211bb9c
aidangaetano11 <aidan.gaetano@wsu.edu>
2024-03-26 17:44:59 -0700

    Moved points to calibration page

--- src/pages/calibration.tsx
+++ src/pages/calibration.tsx
@@ -6,6 +6,7 @@ import {useRef, useEffect, useState, useContext} from 'react'
 import React from 'react'
 import BoxContainer from '../box_container'
 import {OneEuroFilter} from '1eurofilter'
+import * as d3 from "d3";
 
 
 declare global {
@@ -24,6 +25,7 @@ function Calibration() {
     const leftEyeRef = useRef<any>(null);
     const rightEyeRef = useRef<any>(null);
     const headRef = useRef<any>(null);
+    const vectorCalibRef = useRef<any>(null);
 
 
     const connect = window.drawConnectors;
@@ -87,7 +89,59 @@ function Calibration() {
 
     }, [leftIrisCoordinate, rightIrisCoordinate, calibrationPoints]); // These are our dependent variables
 
+    useEffect(() => {
+      // set SVG
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const svg = d3.select(vectorCalibRef.current)
+                    .attr("width", width)
+                    .attr("height", height);
+      
+      
+      // Calculate intervals for 4x4 grid
+      const cols = 4;
+      const rows = 4;
+      const intervalX = width / (cols - 1);
+      const intervalY = height / (rows - 1);
 
+      // Generate random vectors
+      const data = [];
+      for (let i = 0; i < cols; i++){
+          for (let j = 0; j < rows; j++){
+              data.push({
+                  x: i * intervalX,
+                  y: j * intervalY,
+                  dx: Math.random() * 20 - 10,
+                  dy: Math.random() * 20 - 10,
+              });
+          }
+      }
+
+      // Draw vector field
+      svg.selectAll(".vector")
+         .data(data)
+         .enter()
+         .append("line") // Draw line
+         .attr("x1", d => d.x)
+         .attr("y1", d => d.y)
+         .attr("x2", d => d.x + d.dx)
+         .attr("y2", d => d.y + d.dy)
+         .attr("stroke", "red")
+         .attr("marker-end", "url(#arrow)"); // Add arrow
+      // Def of arrow
+      svg.append("defs")
+         .append("marker")
+         .attr("id", "arrow")
+         .attr("viewBox", "0 -5 10 10")
+         .attr("refX", 5)
+         .attr("refY", 0)
+         .attr("markerWidth", 6)
+         .attr("markerHeight", 6)
+         .attr("orient", "auto")
+         .append("path")
+         .attr("d", "M0,-5L10,0L0,5")
+         .attr("fill", "red");
+  }, []);
     
 
     // This function will get the line of best fit between all of our points
@@ -368,6 +422,7 @@ function Calibration() {
       
   return (
     <div>
+      
       <canvas
         ref={crosshairCanvasRef}
         width="1920"
@@ -399,7 +454,7 @@ function Calibration() {
           cursor: 'crosshair',
         }}
       />
-
+      <svg ref={vectorCalibRef}></svg>
       <Webcam 
         ref={webcamRef}
           style={{
